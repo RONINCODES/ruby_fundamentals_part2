f5e9d3c94641a1c3327242f0dc10bd980b30d367
Ronen Annason <ronenannason83@gmail.com>
2017-05-16 14:59:25 -0400

    exercise 0 comlpleted

---
+++ exercise1.rb
@@ -0,0 +1,20 @@
+fav_colours = ['blue', 'green', 'yellow', 'white']
+age = [21, 33, 31, 20, 22]
+coin = ["heads", "heads", "heads", "tails"]
+performing_artists = ["immortal technique", "Snoop", "Julio Eglesias", "Nas" ]
+fav_colors = [:blue, :green, :yellow, :white]
+
+definitions = {
+  :sibling => "sistr or brother",
+  :parent => "mom or dad",
+  :school => "a place of learning"}
+
+fav_mov = {:rocky => 1989, :godfather =>1970, :Jaws =>1983}
+cities = {:Toronto => 2000000, :Montreal => 1000000, :Moscow => 12400000}
+
+siblings_cousins_friends = {
+  :masha => { :age => 21, :relationship => "friend"},
+  :karina => {:age => 31, :relationship => "sister"},
+  :don => {:age => 39, :relationship => "friend"},
+  :bob => {:age => 50, :relationship => "cousin"}
+}
